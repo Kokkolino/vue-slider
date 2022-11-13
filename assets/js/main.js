61936865ac859f8f87a5d1121e68a88152166b01
Francesco C <fran461j@outlook.it>
2022-11-13 17:02:53 +0100

    slide right and left

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 var app = new Vue({
     el: '#root',
     data: {
-      message: 'Hello Vue!',
-      imgs:[
-        {id: 0, name: 'bird', url: './assets/img/bird.jpg'},
-        {id: 1, name: 'car', url: './assets/img/cat.jpg'},
-        {id: 2, name: 'horses', url: './assets/img/horses.jpg'},
-        {id: 3, name: 'meditate', url: './assets/img/meditate.jpg'},
-        {id: 4, name: 'train', url: './assets/img/train.jpg'},
-      ]
+      imgObj:[
+        {id: 0, name: 'bird', source: './assets/img/bird.jpg'},
+        {id: 1, name: 'car', source: './assets/img/cat.jpg'},
+        {id: 2, name: 'horses', source: './assets/img/horses.jpg'},
+        {id: 3, name: 'meditate', source: './assets/img/meditate.jpg'},
+        {id: 4, name: 'train', source: './assets/img/train.jpg'},
+      ],
+      index: 0,
+    },
+    methods: {
+        slideLeft: function(){
+            const max = this.imgObj.length - 1;
+            console.log(max)
+            if(this.index == 0){
+                return this.index = max;
+            }
+            return this.index--;
+        },
+        slideRight: function(){
+            const max = this.imgObj.length - 1;
+            if(this.index == max){
+                return this.index = 0;
+            }
+            return this.index++;
+        }
+
     }
   })
