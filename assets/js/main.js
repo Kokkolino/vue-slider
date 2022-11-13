85e7269acd68fb294154f79bfd6fe9a8832933e7
Francesco C <fran461j@outlook.it>
2022-11-13 16:16:59 +0100

    assets and obj

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 var app = new Vue({
-    el: '#app',
+    el: '#root',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      imgs:[
+        {id: 0, name: 'bird', url: './assets/img/bird.jpg'},
+        {id: 1, name: 'car', url: './assets/img/cat.jpg'},
+        {id: 2, name: 'horses', url: './assets/img/horses.jpg'},
+        {id: 3, name: 'meditate', url: './assets/img/meditate.jpg'},
+        {id: 4, name: 'train', url: './assets/img/train.jpg'},
+      ]
     }
   })
